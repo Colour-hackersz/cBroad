07cd892bf50dea0a295cd75356a8b24cff121749
Colour-hackersz <amatlib62@gmail.com>
2026-03-19 14:07:51 +0530

    Change BOT_TOKEN to a specific value
    Updated BOT_TOKEN to a specific token for the bot.

--- brbot.js
+++ brbot.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { BaseScene, Stage } = Scenes;
 
 /* ---------------- BOT TOKEN ---------------- */
-const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "YOUR_TOKEN";
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "8662842894:AAFfSklFWQCDkCVV-u4ktn2VsraU4DF6ECc";
 if (!BOT_TOKEN) process.exit(1);
 
 const bot = new Telegraf(BOT_TOKEN);
